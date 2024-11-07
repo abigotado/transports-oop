51dd3cd5c341ed2a56d510bb23c8dc85dc22d09e
Nikita Kovalenko <nik.koval.89@gmail.com>
2024-11-07 19:56:49 -0300

    Create tram and express train

--- src/main/java/org/abigotado/app/transport/RailVehicle.java
+++ src/main/java/org/abigotado/app/transport/RailVehicle.java
@@ -1,9 +1,11 @@
 package org.abigotado.app.transport;
 
 import org.abigotado.app.enums.EngineType;
+import org.abigotado.app.enums.FuelType;
 import org.abigotado.app.interfaces.ElectricPowered;
+import org.abigotado.app.interfaces.FuelPowered;
 
-sealed abstract class RailVehicle extends Vehicle permits Metro {
+sealed abstract class RailVehicle extends Vehicle permits ExpressTrain, Metro, Tram {
     protected EngineType engineType;
 
     public RailVehicle(String name, double speed, EngineType engineType) {
@@ -29,3 +31,44 @@ final class Metro extends RailVehicle implements ElectricPowered {
         System.out.printf("Metro: %s, Speed: %.2f km/h, Engine: %s%n", name, speed, engineType);
     }
 }
+
+final class ExpressTrain extends RailVehicle implements FuelPowered {
+    public ExpressTrain(String name, double speed) {
+        super(name, speed, EngineType.INTERNAL_COMBUSTION);
+    }
+
+    @Override public void move() {
+        System.out.println("The express train " + name + " is moving on the rails.");
+    }
+
+    @Override public void displayInfo() {
+        System.out.printf("ExpressTrain: %s, Speed: %.2f km/h, Engine: %s%n", name, speed, engineType);
+    }
+
+    @Override public FuelType getFuelType() {
+        return FuelType.DIESEL;
+    }
+
+    @Override public void refuel() {
+        System.out.println("Refueling the express train with " + FuelType.DIESEL);
+    }
+}
+
+final class Tram extends RailVehicle implements ElectricPowered {
+    public Tram(String name, double speed) {
+        super(name, speed, EngineType.ELECTRIC);
+    }
+
+    @Override public void move() {
+        System.out.println("The tram " + name + " is moving on the rails.");
+    }
+
+    @Override public void displayInfo() {
+        System.out.printf("Tram: %s, Speed: %.2f km/h, Engine: %s%n", name, speed, engineType);
+    }
+
+    @Override public void charge() {
+        System.out.println("Charging the tram.");
+    }
+}
+
