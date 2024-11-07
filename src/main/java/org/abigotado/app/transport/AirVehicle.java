3f9d0797aa5885247a19135a2f1d945c46ad7e95
Nikita Kovalenko <nik.koval.89@gmail.com>
2024-11-07 19:21:59 -0300

    Create airplane

--- src/main/java/org/abigotado/app/transport/AirVehicle.java
+++ src/main/java/org/abigotado/app/transport/AirVehicle.java
@@ -1,6 +1,8 @@
 package org.abigotado.app.transport;
 
 import org.abigotado.app.enums.EngineType;
+import org.abigotado.app.enums.FuelType;
+import org.abigotado.app.interfaces.FuelPowered;
 
 sealed abstract class AirVehicle extends Vehicle {
     protected EngineType engineType;
@@ -8,5 +10,35 @@ sealed abstract class AirVehicle extends Vehicle {
     public AirVehicle(String name, double speed, EngineType engineType) {
         super(name, speed);
         this.engineType = engineType;
+    }
+}
+
+final class Airplane extends AirVehicle implements FuelPowered {
+    private final FuelType fuelType;
+
+    public Airplane(String name, double speed) {
+        super(name, speed, EngineType.JET);
+        this.fuelType = FuelType.KEROSENE;
+    }
+
+    @Override
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling the airplane with kerosene.");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("The airplane is flying in the sky.");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("Transport: %s, Speed: %.2f km/h, Engine: %s, Fuel: %s%n",
+                          name, speed, engineType, fuelType);
     }
 }
