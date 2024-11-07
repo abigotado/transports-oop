0daba1b3260c17473d4c31a65cda01b037b4cb17
Nikita Kovalenko <nik.koval.89@gmail.com>
2024-11-07 19:50:28 -0300

    Create metro

--- src/main/java/org/abigotado/app/transport/RailVehicle.java
+++ src/main/java/org/abigotado/app/transport/RailVehicle.java
@@ -1,12 +1,35 @@
 package org.abigotado.app.transport;
 
 import org.abigotado.app.enums.EngineType;
+import org.abigotado.app.interfaces.ElectricPowered;
 
-sealed abstract class RailVehicle extends Vehicle {
+sealed abstract class RailVehicle extends Vehicle permits Metro {
     protected EngineType engineType;
 
     public RailVehicle(String name, double speed, EngineType engineType) {
         super(name, speed);
         this.engineType = engineType;
+    }
+}
+
+ final class Metro extends RailVehicle implements ElectricPowered {
+    public Metro(String name, double speed) {
+        super(name, speed, EngineType.ELECTRIC);
+    }
+
+    @Override
+    public void charge() {
+        System.out.println("Charging the metro train.");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("The metro is moving underground.");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("Metro: %s, Speed: %.2f km/h, Engine: %s%n",
+                          name, speed, engineType);
     }
 }
