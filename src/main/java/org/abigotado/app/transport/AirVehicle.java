c961d9097485ea4f105658a1c579bf87ec36029b
Nikita Kovalenko <nik.koval.89@gmail.com>
2024-11-07 19:46:41 -0300

    Create air balloon

--- src/main/java/org/abigotado/app/transport/AirVehicle.java
+++ src/main/java/org/abigotado/app/transport/AirVehicle.java
@@ -4,7 +4,7 @@ import org.abigotado.app.enums.EngineType;
 import org.abigotado.app.enums.FuelType;
 import org.abigotado.app.interfaces.FuelPowered;
 
-sealed abstract class AirVehicle extends Vehicle {
+sealed abstract class AirVehicle extends Vehicle permits Airplane, Helicopter, AirBalloon {
     protected EngineType engineType;
 
     public AirVehicle(String name, double speed, EngineType engineType) {
@@ -21,25 +21,20 @@ final class Airplane extends AirVehicle implements FuelPowered {
         this.fuelType = FuelType.KEROSENE;
     }
 
-    @Override
-    public FuelType getFuelType() {
+    @Override public FuelType getFuelType() {
         return fuelType;
     }
 
-    @Override
-    public void refuel() {
+    @Override public void refuel() {
         System.out.println("Refueling the airplane with kerosene.");
     }
 
-    @Override
-    public void move() {
+    @Override public void move() {
         System.out.println("The airplane is flying in the sky.");
     }
 
-    @Override
-    public void displayInfo() {
-        System.out.printf("Airplane: %s, Speed: %.2f km/h, Engine: %s, Fuel: %s%n",
-                          name, speed, engineType, fuelType);
+    @Override public void displayInfo() {
+        System.out.printf("Airplane: %s, Speed: %.2f km/h, Engine: %s, Fuel: %s%n", name, speed, engineType, fuelType);
     }
 }
 
@@ -51,24 +46,38 @@ final class Helicopter extends AirVehicle implements FuelPowered {
         this.fuelType = FuelType.KEROSENE;
     }
 
-    @Override
-    public FuelType getFuelType() {
+    @Override public FuelType getFuelType() {
         return fuelType;
     }
 
-    @Override
-    public void refuel() {
+    @Override public void refuel() {
         System.out.println("Refueling the helicopter with kerosene.");
     }
 
-    @Override
-    public void move() {
+    @Override public void move() {
         System.out.println("The helicopter is hovering in the air.");
     }
 
-    @Override
-    public void displayInfo() {
+    @Override public void displayInfo() {
         System.out.printf("Helicopter: %s, Speed: %.2f km/h, Engine: %s, Fuel: %s%n",
-                          name, speed, engineType, fuelType);
+                          name,
+                          speed,
+                          engineType,
+                          fuelType
+        );
+    }
+}
+
+final class AirBalloon extends AirVehicle {
+    public AirBalloon(String name, double speed) {
+        super(name, speed, EngineType.NONE);
+    }
+
+    @Override public void move() {
+        System.out.println("The hot air balloon drifts through the sky with the wind.");
+    }
+
+    @Override public void displayInfo() {
+        System.out.printf("AirBalloon: %s, Speed: %.2f km/h, Engine: %s%n", name, speed, engineType);
     }
 }
